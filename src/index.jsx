925446e032f6c5e518709dcc2ecd19d23f37a206
Max Scopp <m.scopp32@gmail.com>
2018-06-18 09:33:27 +0200

    Added imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { ContactBox } from "./ContactBox";
+import { CookieBanner } from "./CookieBanner";
+
 const App = () => (
   <div>
     <ContactBox />
